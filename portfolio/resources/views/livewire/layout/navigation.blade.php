29af7c5f6d60d822a84f63f707abed1aa8f694b3
chryss-portfolio <the.chryss.portfolio@gmail.com>
2024-11-13 00:58:50 +0800

    fixed ui

--- portfolio/resources/views/livewire/layout/navigation.blade.php
+++ portfolio/resources/views/livewire/layout/navigation.blade.php
@@ -75,7 +75,7 @@ new class extends Component
                         </x-slot>
                     </x-dropdown>
                 </div>
-
+            @endauth
                 <!-- Hamburger -->
                 <div class="-me-2 flex items-center sm:hidden">
                     <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
@@ -85,7 +85,7 @@ new class extends Component
                         </svg>
                     </button>
                 </div>
-            @endauth
+
         </div>
     </div>
 
